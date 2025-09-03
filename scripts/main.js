54a34fc640cd04c7ab5bebd4e97f5305c25b1820
SergoDevLogs <gurovsergej390@gmail.com>
2025-09-03 13:49:09 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+const listItems = document.querySelectorAll("li")
+
+function toggleDone(e) {
+    if(!e.target.className) {
+        e.target.className = "done";
+    } else {
+        e.target.className = "";
+    }
+}
+
+listItems.forEach((item) => {
+    item.addEventListener("click", toggleDone);
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name");
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
+
+if(!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+const myImage = document.querySelector("img");
+myImage.addEventListener("click", () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc == "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/firefox2.png");
+    } else  {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+
+});
