7846b97a91d5b4d8f477231050b00c01f699123e
SergoDevLogs <gurovsergej390@gmail.com>
2025-09-05 19:21:09 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -12,8 +12,42 @@ listItems.forEach((item) => {
     item.addEventListener("click", toggleDone);
 });
 
-let myButton = document.querySelector("button");
+let myButton = document.querySelector("#usname");
 let myHeading = document.querySelector("h1");
+let scrollUpButton = document.querySelector("#upbutton");
+
+function scrollUp(){
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    });
+}
+
+function scrollDown(){
+    window.scrollTo({
+        top:document.documentElement.scrollHeight,
+        behavior: 'smooth'
+    });
+}
+
+function toggleScroll(){
+    const shouldBeFlipped = window.scrollY < 40;
+    if (window.scrollY < 40) {
+        scrollUpButton.classList.add('flipped');
+    } else {
+        scrollUpButton.classList.remove('flipped');
+    }
+}
+window.addEventListener('scroll', toggleScroll);
+window.addEventListener('load', toggleScroll);
+scrollUpButton.addEventListener("click", () => {
+    if(window.scrollY >= 40) {
+        scrollUp();
+    }
+    else {
+        scrollDown();
+    }
+})
 
 function setUserName(cause) {
     const myName = prompt(cause);
@@ -39,13 +73,13 @@ myButton.addEventListener("click", () => {
 });
 
 if(!localStorage.getItem("name")) {
-    setUserName();
+    setUserName("Введите имя пользователя");
 } else {
     const storedName = localStorage.getItem("name");
     myHeading.textContent = `Поздравляю, ${storedName}, ты - подебил.`;
 }
 
-const myImage = document.querySelector("img");
+const myImage = document.querySelector("#main-logo");
 myImage.addEventListener("click", () => {
     const mySrc = myImage.getAttribute("src");
     if (mySrc == "images/firefox-icon.png") {
